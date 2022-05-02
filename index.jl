fef548434ba871ebb9769f246a4ac99900703350
noeleont <noeleont@users.noreply.github.com>
2022-05-02 20:09:55 +0000

    Deploying to gh-pages from @ lesolution-how/www@b087ff7984a77cc8f85ae51382a3e830c27d2fc6 🚀

---
+++ index.jl
@@ -0,0 +1,32 @@
+### A Pluto.jl notebook ###
+# v0.19.0
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ c6dd0aae-c93e-11ec-2264-013f62efd009
+md"# leSolution"
+
+# ╔═╡ d823ac73-fb80-4189-b656-b01fef646fc6
+md"## Fiesi Projects:"
+
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+julia_version = "1.7.2"
+manifest_format = "2.0"
+
+[deps]
+"""
+
+# ╔═╡ Cell order:
+# ╟─c6dd0aae-c93e-11ec-2264-013f62efd009
+# ╠═d823ac73-fb80-4189-b656-b01fef646fc6
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002
